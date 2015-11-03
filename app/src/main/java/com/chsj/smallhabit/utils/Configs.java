63ce06b03cc11348918b324da9335066a8182b54
jmxgrobby <jmxgrobby@163.com>
2015-11-03 14:21:04 +0800

    2015-11-3 QB 测试是否能push

--- app/src/main/java/com/chsj/smallhabit/utils/Configs.java
+++ app/src/main/java/com/chsj/smallhabit/utils/Configs.java
@@ -6,6 +6,10 @@ package com.chsj.smallhabit.utils;
  * Email: jmxgrobby@163.com
  * Date:  2015/11/3.
  */
+
+/**
+ * 这是一个工具类，包含共享参数的信息：如共享参数名，存储字段的key值等
+ */
 public class Configs {
     //共享参数名
     public static final String SHARDPERFACE_NAME = "sharedPreferences";
